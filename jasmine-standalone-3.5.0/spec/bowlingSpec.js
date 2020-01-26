8476df0e37fceb871fa9bff18e10dd0d918be546
AliciaMerlino <acmerlino1@gmail.com>
2020-01-26 16:41:37 +0000

    score increases after pins hit

--- jasmine-standalone-3.5.0/spec/bowlingSpec.js
+++ jasmine-standalone-3.5.0/spec/bowlingSpec.js
@@ -4,7 +4,13 @@ describe("bowling", function() {
   var bowling;
   
   it("starts at 0 points", function() {
-    var bowling = new Bowling
+    var bowling = new Bowling;
     expect(bowling.score).toEqual(0);
   });
+
+  it("accumulates points", function(){
+    var bowling = new Bowling;
+    bowling.rollOne(5)
+    expect(bowling.score).toEqual(5);
+  })
 });
--- jasmine-standalone-3.5.0/src/bowling.js
+++ jasmine-standalone-3.5.0/src/bowling.js
@@ -1,3 +1,6 @@
 var Bowling = function() {
   this.score = 0
+  this.rollOne = function(pins){
+    this.score += pins;
+  };
 }
